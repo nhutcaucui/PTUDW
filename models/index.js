bce88d56ae7541d0ed24a65db24fc4ef7245a472
vinam <nvnamsss>
2019-06-07 13:48:20 +0700

    set database infor

--- models/index.js
+++ models/index.js
@@ -2,9 +2,9 @@ var self = this;
 const mysql = require('mysql');
 require('./user');
 var database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
+    host: "sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "cv04hmy6cj0pwz6r",
+    password: "pk2fczqoz9g8z6s1"
 });
 
 var db = database.connect((err) =>{
