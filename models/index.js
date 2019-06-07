b0ae456b4f99f8b844630225cb42199fb80625b8
vinam <nvnamsss>
2019-06-07 21:44:38 +0700

    add database in heroku

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 var self = this;
-var db;
+
 const mysql = require('mysql');
 require('./user');
 
 var database = mysql.createConnection({
     host: "sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "cv04hmy6cj0pwz6r",
-    password: "pk2fczqoz9g8z6s1"
+    password: "pk2fczqoz9g8z6s1",
+    database: "rs7drnwynwen7d4a",
 });
 
 // var database = createConnection = () => {
@@ -18,79 +19,14 @@ var database = mysql.createConnection({
 //     });
 // }
 
-// var db = database.connect((err) =>{
-//     if (err){
-//         throw err;
-//     }
-//     console.log("[Database] - Connected");
-// });
+database.connect((err) =>{
+    if (err){
+        throw err;
+    }
+    console.log("[Database] - Connected");
+});
 
 module.exports = {
-    mysql : db,
+    mysql : database,
 }
-  //   load: sql => {
-  //     return new Promise((resolve, reject) => {
-  //       var connection = createConnection();
-  //       connection.connect();
-  //       connection.query(sql, (error, results, fields) => {
-  //         if (error)
-  //           reject(error);
-  //         else {
-  //           resolve(results);
-  //         }
-  //         connection.end();
-  //       });
-  //     });
-  //   },
   
-  //   add: (tableName, entity) => {
-  //     return new Promise((resolve, reject) => {
-  //       var sql = `insert into ${tableName} set ?`;
-  //       var connection = createConnection();
-  //       connection.connect();
-  //       connection.query(sql, entity, (error, value) => {
-  //         if (error)
-  //           reject(error);
-  //         else {
-  //           resolve(value.insertId);
-  //         }
-  //         connection.end();
-  //       });
-  //     });
-  //   },
-  
-  //   update: (tableName, idField, entity) => {
-  //     return new Promise((resolve, reject) => {
-  //       var id = entity[idField];
-  //       delete entity[idField];
-  
-  //       var sql = `update ${tableName} set ? where ${idField} = ?`;
-  //       var connection = createConnection();
-  //       connection.connect();
-  //       connection.query(sql, [entity, id], (error, value) => {
-  //         if (error)
-  //           reject(error);
-  //         else {
-  //           resolve(value.changedRows);
-  //         }
-  //         connection.end();
-  //       });
-  //     });
-  //   },
-  
-  //   delete: (tableName, idField, id) => {
-  //     return new Promise((resolve, reject) => {
-  //       var sql = `delete from ${tableName} where ${idField} = ?`;
-  //       var connection = createConnection();
-  //       connection.connect();
-  //       connection.query(sql, id, (error, value) => {
-  //         if (error)
-  //           reject(error);
-  //         else {
-  //           resolve(value.affectedRows);
-  //         }
-  //         connection.end();
-  //       });
-  //     });
-  //   },
-  // };
