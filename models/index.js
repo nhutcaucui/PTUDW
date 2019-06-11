e9e546e1fa5113af1e2c41d15ac1b2c76d91793b
Nhựt <nhutcaucui@gmail.com>
2019-06-11 22:42:00 +0700

    old query

--- models/index.js
+++ models/index.js
@@ -29,35 +29,70 @@ database.connect((err) =>{
 module.exports = {
     mysql : database,
 
-    // loaddb: sql => {
-    //     return new Promise((resolve, reject) => {
-    //       var connection = createConnection();
-    //       connection.connect();
-    //       connection.query(sql, (error, results, fields) => {
-    //         if (error)
-    //           reject(error);
-    //         else {
-    //           resolve(results);
-    //         }
-    //         connection.end();
-    //       });
-    //     });
-    //   },
+    loaddb: sql => {
+        return new Promise((resolve, reject) => {
+          var connection = createConnection();
+          connection.connect();
+          connection.query(sql, (error, results, fields) => {
+            if (error)
+              reject(error);
+            else {
+              resolve(results);
+            }
+            connection.end();
+          });
+        });
+      },
 
-    // add: (tableName, entity) => {
-    //     return new Promise((resolve, reject) => {
-    //       var sql = `INSERT INTO ${tableName} SET ?`;
-    //       var connection = createConnection();
-    //       connection.connect();
-    //       connection.query(sql, entity, (error, value) => {
-    //         if (error)
-    //           reject(error);
-    //         else {
-    //           resolve(value.insertId);
-    //         }
-    //         connection.end();
-    //       });
-    //     });
-    //   },
+    add: (tableName, entity) => {
+        return new Promise((resolve, reject) => {
+          var sql = `INSERT INTO ${tableName} SET ?`;
+          var connection = createConnection();
+          connection.connect();
+          connection.query(sql, entity, (error, value) => {
+            if (error)
+              reject(error);
+            else {
+              resolve(value.insertId);
+            }
+            connection.end();
+          });
+        });
+      },
+
+      update: (tableName, idField, entity) => {
+        return new Promise((resolve, reject) => {
+          var id = entity[idField];
+          delete entity[idField];
+    
+          var sql = `UPDATE ${tableName} SET ? WHERE ${idField} = ?`;
+          var connection = createConnection();
+          connection.connect();
+          connection.query(sql, [entity, id], (error, value) => {
+            if (error)
+              reject(error);
+            else {
+              resolve(value.changedRows);
+            }
+            connection.end();
+          });
+        });
+      },
+    
+      delete: (tableName, idField, id) => {
+        return new Promise((resolve, reject) => {
+          var sql = `DELETE FROM ${tableName} WHERE ${idField} = ?`;
+          var connection = createConnection();
+          connection.connect();
+          connection.query(sql, id, (error, value) => {
+            if (error)
+              reject(error);
+            else {
+              resolve(value.affectedRows);
+            }
+            connection.end();
+          });
+        });
+      },
 }
   
