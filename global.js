c90986135c10796de92d4d6997732557a5f8dcc4
nguyen vi nam <nam.nguyen@eyeq.tech>
2019-06-15 11:31:21 +0700

    implement profile router, slightly fix datetime util

--- global.js
+++ global.js
@@ -2,6 +2,13 @@ var hot;
 var top;
 var news;
 var cats;
+var tag;
+var tag_manager = {
+    //example
+    //kinhte: 1,
+    //cuocsong: 2,
+};
+
 
 module.exports = {
     hot: hot,
--- routes/profile.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    let username = req.query.username;
-    console.log('[Profile] -', username);
-});
-
-module.exports = router;
